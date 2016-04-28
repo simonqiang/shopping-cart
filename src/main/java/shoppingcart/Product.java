d4f67d8de5d1cdce7834ec07650dd555a856c3b8
simon qiang <simonqiang@gmail.com>
2016-04-28 08:37:49 +0100

    add product class

---
+++ src/main/java/shoppingcart/Product.java
@@ -0,0 +1,45 @@
+package shoppingcart;
+
+import java.util.UUID;
+
+/**
+ * Created by 608502920 on 4/28/2016.
+ */
+public class Product {
+    private String name;
+    private double price;
+    private UUID id;
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
